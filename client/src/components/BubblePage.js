f66b24bc034c3afbaae81af31209bb16956f9aef
kai-blt <blaitong@gmail.com>
2020-11-13 09:43:42 -0800

    added delete and save functionality

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -9,18 +9,22 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
-  useEffect(() => {
+  const getColors = () => {
     axiosWithAuth().get('http://localhost:5000/api/colors')
-      .then(res => {
-        console.log(res.data);
-        setColorList(res.data)
-      })
-      .catch(err => console.log(err));
+    .then(res => {
+      console.log(res.data);
+      setColorList(res.data)
+    })
+    .catch(err => console.log(err));
+  }
+
+  useEffect(() => {
+   getColors();
   }, []);
 
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
+      <ColorList colors={colorList} updateColors={getColors} />
       <Bubbles colors={colorList} />
     </>
   );
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -18,13 +18,25 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    axiosWithAuth().put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        console.log(res.data);
+        updateColors();
+      })
+      .catch(err => {
+        console.log(err);
+      })
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+    axiosWithAuth().delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(res => {
+        console.log(res.data);
+        updateColors();
+      })
+      .catch(err => {
+        console.log(err);
+      })
   };
 
   return (
