d10a637f4b3e5af4289596abb30196c2b467e32c
kai-blt <blaitong@gmail.com>
2020-11-13 10:32:14 -0800

    completed component test

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -6,22 +6,46 @@ import BubblePage from "./BubblePage";
 import { getColors as mockGetColors } from '../api/getColors';
 jest.mock('../api/getColors');
 
-
-const mockData = [
-  {
-    color: 'red',
-    code: {hex: '#fff'},
-    id: 1
-  }
-]
+//Set up mock respose payload
+const mockData = {
+  data: [
+    {
+      color: 'red',
+      code: {hex: '#FF0000'},
+      id: 1
+    },
+    {
+      color: 'green',
+      code: {hex: '#00FF00'},
+      id: 2
+    },
+    {
+      color: 'blue',
+      code: {hex: '#0000FF'},
+      id: 3
+    },
+  ],
+};
 
 test("Fetches data and renders the bubbles", async () => {
+  //Resolve the call once so Bubble Page has data to render
   mockGetColors.mockResolvedValueOnce(mockData);
 
+  //Render bubble page
   render(<BubblePage />)
 
   await waitFor(() => {
-    const color = screen.queryByText(/red/i);
+    //capture bubbles from the DOM
+    const redBubble = screen.queryByText(/red/i);
+    const greenBubble = screen.queryByText(/green/i);
+    const blueBubble = screen.queryByText(/blue/i);
+
+    //Assert they exist!
+    expect(redBubble).toBeInTheDocument();
+    expect(greenBubble).toBeInTheDocument();
+    expect(blueBubble).toBeInTheDocument();
+    
+    //Check out human readable prinout of DOM
     screen.debug();
   })
 });
